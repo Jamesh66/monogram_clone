5fff0ac9893dbc73e905439277f1cc6f38cf124c
James Harrold <68052093+Jamesh66@users.noreply.github.com>
2023-10-08 22:36:54 +0100

    featured cta block

--- Developer/training-repos/Monogram/monogram/src/App.js
+++ Developer/training-repos/Monogram/monogram/src/App.js
@@ -6,11 +6,13 @@ import './global/Fonts.scss'
 import './global/Global.scss'
 import './components/toggle-button/toggleButton.scss'
 import './components/featured-items/featuredItems.scss'
+import "./components/featured-cta/featuredCta.scss"
 
 //Components
 import Nav from "../src/components/nav/Nav";
 import Banner from "../src/components/banner/Banner";
 import FeaturedItems from './components/featured-items/featuredItems';
+import FeaturedCta from './components/featured-cta/featuredCta';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       <Nav/>
       <Banner/>
       <FeaturedItems/>
+      <FeaturedCta/>
     </div>
   );
 }
---
+++ Developer/training-repos/Monogram/monogram/src/components/featured-cta/featuredCta.js
@@ -0,0 +1,12 @@
+//Import images 
+import shopBannerModule1920 from "../../assets/images/shop-banner-module-1920.webp"
+import shopBannerModule768 from "../../assets/images/shop-banner-module-768.webp"
+
+const featuredCta = () => {
+  return (
+    <div className="featuredCta">
+    </div>
+  );
+};
+
+export default featuredCta;
